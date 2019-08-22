e5a02b01ca5fd99a20b7abb8c6c2e8f2e2a3e53c
tsukuda <v_mayo.tukuda@pactera.com>
2019-08-22 17:37:49 +0900

    add radar-chart/App.js

--- recipe_level/count_ingredients.py
+++ recipe_level/count_ingredients.py
@@ -1,10 +1,11 @@
 import os
 import json
 
-
+_HOME = os.path.expanduser("~")
 _INGREDIENTS_DIR = '../recipe_conv/weekcook/ingredient_json'
 _SENTENCES_DIR = './rne_wakachi'
 _ACTIONS_DIR = './countAC'
+_RECIPEDB_DIR = os.path.join(_HOME, 'var/data/recipe/weekcook/recipe_root')
 
 _OUTPUT_DIR = './num_of_params'
 
@@ -27,8 +28,25 @@ def output_jsondata(*args) -> str:
     print('args', args)
     output = [d for d in args]
     print('output : ', output)
-    
+
     return json.dumps(output)
+
+
+def fetch_recipename(json_file: str) -> str:
+    with open(json_file, 'r', encoding='utf-8') as r:
+        input_data = json.load(r)
+        recipe_name = input_data['Recipe_Infos'][0]['recipe_name']
+        print(recipe_name)
+
+    return recipe_name
+
+
+def compute_mean(dict_data: dict) -> float:
+    all_value = [v for v in dict_data.values() if type(v) is not str]
+    length_all_value = len(all_value)
+    mean_value = float(sum(all_value) / length_all_value)
+
+    return mean_value
 
 
 def main():
@@ -36,48 +54,74 @@ def main():
         os.makedirs(_OUTPUT_DIR)
     else:
         pass
-    
+
     file_list = os.listdir(_INGREDIENTS_DIR)
-    print('file_list')
-    print(file_list)
 
+    # ---------------- #
+    # define index key #
+    # ---------------- #
+    recipename_dict = {'key': 'recipename'}
+    ingredints_dict = {'key': 'ingredients'}
+    sentences_dict = {'key': 'sentences'}
+    actions_dict = {'key': 'actions'}
     for f in file_list:
+        print(f)
+        # --------------- #
+        # define filepath #
+        # --------------- #
+        # ingredients path
         read_ingredients_path = os.path.join(_INGREDIENTS_DIR, f)
-
+        read_key_fname, _ = os.path.splitext(f)
+        # sentences path
         sentences_fname, _ = os.path.splitext(f)
         read_sentences_fname = sentences_fname + '_ner_result.txt'
         read_sentences_path = os.path.join(
             _SENTENCES_DIR, read_sentences_fname
         )
- 
+        # actions path
         actions_fname, _ = os.path.splitext(f)
         read_actions_fname = actions_fname + '_ner_result.json'
         read_actions_path = os.path.join(
             _ACTIONS_DIR, read_actions_fname
         )
-        
-        dst_filepath = os.path.join(_OUTPUT_DIR, f)
-        count_ingredients = count_elements(read_ingredients_path)
-        count_ingredients_dict = {
-            'number_of_ingredients': count_ingredients
-        }
-        count_sentences = count_lf(read_sentences_path)
-        count_sentences_dict = {
-            'number_of_sentences': count_sentences
-        }
-        count_actions = count_elements(read_actions_path)
-        count_actions_dict = {
-            'number_of_actions': count_actions
-        }
-        output_data = output_jsondata(
-            count_ingredients_dict,
-            count_sentences_dict,
-            count_actions_dict,
-        )
-        data = json.loads(output_data)
-        with open(dst_filepath, 'w', encoding='utf-8') as w:
-            json.dump(data, w, indent=4, ensure_ascii=False)
+        # recipedb(recipename) path
+        read_recipedb_path = os.path.join(_RECIPEDB_DIR, f)
 
-            
+        # ------------------ #
+        # fetch target value #
+        # ------------------ #
+        target_recipename = fetch_recipename(read_recipedb_path)
+        count_ingredients = count_elements(read_ingredients_path)
+        count_sentences = count_lf(read_sentences_path)
+        count_actions = count_elements(read_actions_path)
+
+        recipename_dict.update({read_key_fname: target_recipename})
+        ingredints_dict.update({read_key_fname: count_ingredients})
+        sentences_dict.update({read_key_fname: count_sentences})
+        actions_dict.update({read_key_fname: count_actions})
+
+    # compute mean each objects
+    ingredients_score_mean = compute_mean(ingredints_dict)
+    sentences_score_mean = compute_mean(sentences_dict)
+    actions_score_mean = compute_mean(actions_dict)
+
+    # add mean to each object dict
+    ingredints_dict.update({'mean': ingredients_score_mean})
+    sentences_dict.update({'mean': sentences_score_mean})
+    actions_dict.update({'mean': actions_score_mean})
+
+    # output data
+    output_data = output_jsondata(
+        recipename_dict,
+        ingredints_dict,
+        sentences_dict,
+        actions_dict,
+    )
+    data = json.loads(output_data)
+    dst_filepath = os.path.join(_OUTPUT_DIR, 'radar-chart.json')
+    with open(dst_filepath, 'w', encoding='utf-8') as w:
+        json.dump(data, w, indent=4, ensure_ascii=False)
+
+
 if __name__ == '__main__':
     main()
--- recipe_level/test_count_ingredients.py
+++ recipe_level/test_count_ingredients.py
@@ -1,13 +1,17 @@
+import os
+import json
 from count_ingredients import count_elements
 from count_ingredients import output_jsondata
 from count_ingredients import count_lf
+from count_ingredients import fetch_recipename
+from count_ingredients import compute_mean
 
 
 def test_count_elements():
     test_file = '../recipe_conv/weekcook/ingredient_json/recipe_2.json'
     expected = 4
     result = count_elements(test_file)
-    
+
     assert result == expected
 
 
@@ -35,6 +39,35 @@ def test_output_json():
         test_sentences_dict,
         test_actions_dict,
     )
-    
+
     assert result == expected
-    
+
+
+def test_fetch_recipename():
+    _HOME = os.path.expanduser("~")
+    _RECIPEDB_DIR = os.path.join(_HOME, 'var/data/recipe/weekcook/recipe_root')
+    test_file = os.path.join(_RECIPEDB_DIR, 'recipe_2.json')
+    expected = 'あげとわかめのお味噌汁'
+    result = fetch_recipename(test_file)
+
+    assert result == expected
+
+
+def test_compute_mean():
+    test_data = {
+        'key': 'key',
+        '0': 0,
+        '1': 0,
+        '2': 0,
+        '3': 0,
+        '4': 0,
+        '5': 10,
+        '6': 10,
+        '7': 10,
+        '8': 10,
+        '9': 10,
+    }
+    expected = 5
+    result = compute_mean(test_data)
+
+    assert result == expected
